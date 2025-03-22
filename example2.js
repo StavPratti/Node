fba44bd90a5896ce333694ebf5799f1d535b4a70
spratth <stavroulaprt@gmail.com>
2025-03-22 18:59:32 +0200

    node.js

---
+++ example2.js
@@ -0,0 +1,18 @@
+const http = require('node:http');
+// for libs
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req) {
+    console.log("A request");
+    res.statusCode =200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  }
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server runing at http://${hostname}:${port}/`);
+})
